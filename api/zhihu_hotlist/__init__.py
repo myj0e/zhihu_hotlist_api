dbfef2d292e70ec5bc72518e4da5f0e40795ceb9
Hoop_lab <542813407@qq.com>
2019-07-07 10:42:36 +0800

    一点微小的工作

--- api/zhihu_hotlist/__init__.py
+++ api/zhihu_hotlist/__init__.py
@@ -5,7 +5,7 @@ import json
 import brotli
 
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(__file__))
 
 import config
 from data_structure import entry
